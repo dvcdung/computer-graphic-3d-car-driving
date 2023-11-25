a5d39d670fcd803539ab926b47be31a533af7a89
dvcdung <dvcvochidung@gmail.com>
2023-11-25 09:13:10 +0700

    john changed

--- src/game/game_manager.py
+++ src/game/game_manager.py
@@ -1,7 +1,7 @@
 import pygame
-from game.ball import Ball
-from game.paddle import Paddle
-from game.road import Road
+from pygame.locals import *
+from game.components.ufo import UFO
+from game.components.meteorite import Meteorite
 
 class GameManager:
     def __init__(self, width, height):
@@ -10,57 +10,105 @@ class GameManager:
         self.game_over = False
         self.clock = pygame.time.Clock()
 
-        # Tạo đối tượng viên bi và thanh điều khiển
-        self.ball = Ball(width // 2, height // 2)
-        self.paddle = Paddle(width // 2 - 50, height - 20)
+        self.x_rotate = 0
+        self.y_rotate = 0 
+        self.z_rotate = 0
+        self.x_translate = 0
+        self.y_translate = 0
+        self.z_translate = 0
+        self.move_unit = 0.01
+        self.rotate_unit = 0.02
         
-        # Tạo đối tượng Road
-        road_width = 400
-        road_height = 20
-        road_color = (128, 128, 128)  # Màu xám
-        self.road = Road((width - road_width) // 2, height - road_height, road_width, road_height, road_color)
+        self.ufo = UFO (self.x_translate, self.y_translate, self.z_translate, self.x_rotate, self.y_rotate, self.z_rotate)
+        self.meteorite = Meteorite(1, 1, -10, 0, 0, 0)
 
     def handle_events(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.paddle.move_left()
-        if keys[pygame.K_RIGHT]:
-            self.paddle.move_right()
+            # Điều khiển camera với các phím mũi tên và các phím số
+            keys = pygame.key.get_pressed()
+            if keys[K_a]:
+                if self.z_rotate < 1:
+                    if self.z_rotate > 1:
+                        self.z_rotate = 1
+                        pass
+                    self.z_rotate += self.rotate_unit
+            if keys[K_d]:
+                if self.z_rotate > -1:
+                    if self.z_rotate < -1:
+                        self.z_rotate = -1
+                        pass
+                    self.z_rotate -= self.rotate_unit
+            if not keys[K_a] and not keys[K_d] and self.z_rotate != 0:
+                    i = self.z_rotate/abs(self.z_rotate)
+                    if int(self.z_rotate/self.rotate_unit*5) == 0:
+                        self.z_rotate = 0
+                        pass
+                    self.z_rotate = (abs(self.z_rotate) - self.rotate_unit)*i
+
+            if keys[K_UP]:
+                if self.x_rotate > -1:
+                    if self.x_rotate < -1:
+                        self.x_rotate = -1
+                        pass
+                    self.x_rotate -= self.rotate_unit
+            if keys[K_DOWN]:
+                if self.x_rotate < 1:
+                    if self.x_rotate > 1:
+                        self.x_rotate = 1
+                        pass
+                    self.x_rotate += self.rotate_unit
+            if not keys[K_UP] and not keys[K_DOWN] and self.x_rotate != 0:
+                    i = self.x_rotate/abs(self.x_rotate)
+                    if int(self.x_rotate/self.rotate_unit*5) == 0:
+                        self.x_rotate = 0
+                        pass
+                    self.x_rotate = (abs(self.x_rotate) - self.rotate_unit)*i
+
+            if keys[K_LEFT]:
+                if self.y_rotate > -1:
+                    if self.y_rotate < -1:
+                        self.y_rotate = -1
+                        pass
+                    self.y_rotate -= self.rotate_unit
+            if keys[K_RIGHT]:
+                if self.y_rotate < 1:
+                    if self.y_rotate > 1:
+                        self.y_rotate = 1
+                        pass
+                    self.y_rotate += self.rotate_unit
+            if not keys[K_LEFT] and not keys[K_RIGHT] and self.y_rotate != 0:
+                    i = self.y_rotate/abs(self.y_rotate)
+                    if int(self.y_rotate/self.rotate_unit*5) == 0:
+                        self.y_rotate = 0
+                        pass
+                    self.y_rotate = (abs(self.y_rotate) - self.rotate_unit)*i
+                    
+            if keys[K_w]:
+                if self.z_translate < 1:
+                    if self.y_rotate > 1:
+                        self.y_rotate = 1
+                        pass
+                    self.z_translate += self.move_unit
+            elif not keys[K_SPACE]:
+                if self.z_translate > 0.0:
+                    self.z_translate -= self.move_unit*2
+                    if self.z_translate < 0.0: self.z_translate = 0.0
+            if keys[K_SPACE]:
+                if self.z_translate > 0.0:
+                    self.z_translate -= self.move_unit
+                    if self.z_translate < 0.0: self.z_translate = 0.0
 
     def update(self):
-        # Cập nhật trạng thái viên bi và thanh điều khiển
-        self.ball.update()
-        self.paddle.update()
-
-        # Kiểm tra va chạm giữa viên bi và thanh điều khiển
-        if pygame.sprite.collide_rect(self.ball, self.paddle):
-            self.ball.bounce()
-
-        # Kiểm tra va chạm với biên trên và dưới
-        if self.ball.rect.top <= 0 or self.ball.rect.bottom >= self.height:
-            self.ball.bounce()
-
-        # Kiểm tra va chạm với đối tượng Road
-        if pygame.sprite.collide_rect(self.ball, self.road):
-            self.ball.bounce()
-
-        # Kiểm tra game over
-        if self.ball.rect.top > self.height:
-            self.game_over = True
-
-    def draw(self, screen):
-        # Vẽ viên bi và thanh điều khiển lên màn hình
-        screen.fill((255, 255, 255))
-        screen.blit(self.ball.image, self.ball.rect)
-        screen.blit(self.paddle.image, self.paddle.rect)
-
-        # Vẽ đối tượng Road lên màn hình
-        screen.blit(self.road.image, self.road.rect)
-
-        # Vẽ biên của cửa sổ game
-        pygame.draw.line(screen, (0, 0, 0), (0, self.height - 1), (self.width, self.height - 1), 2)
-
-        # Hiển thị điểm số (có thể thêm vào nếu muốn)
-
-        # Giới hạn tốc độ frame
-        self.clock.tick(60)
+        self.meteorite.update()
+        self.meteorite.x -= self.ufo.speed[0]*self.x_translate*0.002
+        self.meteorite.y -= self.ufo.speed[1]*self.y_translate*0.002
+        self.meteorite.z += self.ufo.speed[2]*self.z_translate*0.002
+        self.meteorite.x_rotate += self.x_rotate*0.5
+        self.meteorite.y_rotate += self.y_rotate
+        self.meteorite.z_rotate += self.z_rotate*0.5
+        self.ufo.update()
+        # self.ufo.x += self.ufo.speed[0]*self.x_translate
+        # self.ufo.y += self.ufo.speed[1]*self.y_translate
+        # self.ufo.z -= self.ufo.speed[2]*self.z_translate
+        self.ufo.x_rotate += self.x_rotate
+        self.ufo.y_rotate += self.y_rotate
+        self.ufo.z_rotate += self.z_rotate
--- src/main.py
+++ src/main.py
@@ -1,32 +1,54 @@
 import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
 from game.game_manager import GameManager
+import pyrr
 
 # Khởi tạo Pygame
 pygame.init()
 
 # Cài đặt cửa sổ game
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Bắn viên bi")
+width, height = 1600, 900
+pygame.display.set_mode((width, height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption("UFO")
+
+# Khởi tạo camera
+glMatrixMode(GL_PROJECTION)
+projection_matrix = pyrr.matrix44.create_perspective_projection_matrix(45, width/height, 0.1, 50.0)
+glLoadMatrixf(projection_matrix)
+glMatrixMode(GL_MODELVIEW)
+glTranslatef(0.0, -0.06, -1)
+# glTranslatef(0.0, -0.055, -0.03)
 
 # Tạo đối tượng GameManager
 game_manager = GameManager(width, height)
 
 # Chạy game
-while not game_manager.game_over:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_manager.game_over = True
+            pygame.quit()
+            quit()
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    
+    # Thay đổi góc nhìn
+    # glRotatef(game_manager.y_rotate, 0, 1, 0)
+    # glRotatef(game_manager.x_rotate, 1, 0, 0)
+    # glRotatef(game_manager.z_rotate, 0, 0, 1)
+
+    # Di chuyển camera theo trục Z
+    # glTranslatef(0.0, 0.0, -game_manager.z_translate)
 
     # Xử lý sự kiện và cập nhật trạng thái game
     game_manager.handle_events()
     game_manager.update()
 
     # Vẽ đối tượng lên màn hình
-    game_manager.draw(screen)
+    # game_manager.draw()
 
     # Cập nhật màn hình
-    pygame.display.update()
-
-# Kết thúc game
-pygame.quit()
+    pygame.display.flip()
+    pygame.time.wait(10)
